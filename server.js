666db792d1ed961102ce284c0646e81734438a10
Sayain-Aditya <as4316664@gmail.com>
2026-03-17 16:52:54 +0530

    asd

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5000",
-  "https://recipiemakerbackend-nine.vercel.app",
+  "https://recipiemakerbackend-oy2o.vercel.app",
   "https://recipiemakerfrontend-pied.vercel.app"
 ];
 
