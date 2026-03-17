a5a9d23cb0ee052482054fdc1425d0e5b6598040
Sayain-Aditya <as4316664@gmail.com>
2026-03-17 13:45:11 +0530

    env

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5000",
-  "https://recipiemakerbackend.vercel.app",
+  "https://recipiemakerbackend-nine.vercel.app",
   "https://recipiemakerfrontend.vercel.app"
 ];
 app.use(
